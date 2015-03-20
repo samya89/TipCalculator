69d8b49484b4751a59163e77064c4d3a54d0362d
Samya <samya89@gmail.com>
2015-03-20 12:12:27 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Samia Al Rahmani on 3/20/15.
+//  Copyright (c) 2015 Samia Al Rahmani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TipCalculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TipCalculator
+//
+//  Created by Samia Al Rahmani on 3/20/15.
+//  Copyright (c) 2015 Samia Al Rahmani. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
