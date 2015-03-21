d1f678b26a3fb364980cfad78248b58241bb41dc
Samya <samya89@gmail.com>
2015-03-20 17:34:10 -0700

    initial commit

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -10,18 +10,69 @@
 
 @interface ViewController ()
 
+//@property(nonatomic, readonly, retain) UITextInputMode *textInputMode;
+
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UILabel *tipAmountLabel;
+@property (weak, nonatomic) IBOutlet UITextField *tipPercentageTextField;
+
+@property (nonatomic) NSString *billAmountInput;
+@property (nonatomic) NSString *tipPercentageAmount;
+@property (nonatomic,assign) float calculatedTipAmount;
+
+- (IBAction)calculateTip:(id)sender;
+
+
+
 @end
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.billAmountTextField becomeFirstResponder];
+    
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)calculateTip:(id)sender {
+    NSString *billAmountInput = [self.billAmountTextField text];
+    float billAmount = [billAmountInput floatValue];
+    NSString *tipPercentageAmount = [self.tipPercentageTextField text];
+    float tipValue = [tipPercentageAmount floatValue];
+    float tipPercentage = tipValue / 100;
+    
+    self.calculatedTipAmount = billAmount * tipPercentage;
+    self.tipAmountLabel.text = [NSString stringWithFormat:@"$ %f", self.calculatedTipAmount];
+}
+
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField
+{
+    return YES;
+}
+
+
+-(void)textFieldDidEndEditing:(UITextField *)textField{
+    [textField resignFirstResponder];
+}
+
+
+
+
+
+
+
+
+
+
 
 @end
